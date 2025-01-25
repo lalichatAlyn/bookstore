a2fb1abe4920f238f0c7e18b6c5f7937c69c8325
AlynLalichat <lalichatalyn@gmail.com>
2025-01-25 15:20:38 +0700

    feat:create server with express

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { client } from "./utils/db.js";
+import booksRouter from "./apps/books.js";
+import cartRouter from "./apps/cart.js";
+
+async function init() {
+  const app = express();
+  const port = 4000;
+
+  await client.connect();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
+
+init();
